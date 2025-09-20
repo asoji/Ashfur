43cc2c715ee56493ba09f67ea0d63757f30b2fd9
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-09-20 01:54:09 -0700

    basic project setup

---
+++ Ashfur/Commands/BasicsModule.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using NetCord;
+using NetCord.Rest;
+using NetCord.Services.ApplicationCommands;
+using NetCord.Services.Commands;
+
+namespace Ashfur.Commands;
+
+public class BasicsModule : ApplicationCommandModule<ApplicationCommandContext> {
+    [SlashCommand("about", "About Ashfur")]
+    public async Task About() {
+        Assembly netcordAssembly = typeof(NetCord.Application).Assembly;
+        var netcordVersionAttribute = netcordAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+        string netcordVersion = netcordVersionAttribute?.InformationalVersion ?? "i ate the version";
+
+        EmbedFooterProperties aboutFooter = new EmbedFooterProperties()
+            .WithText("oh for fuck sakes");
+
+        EmbedProperties aboutEmbed = new EmbedProperties()
+            .WithTitle("Ashfur")
+            .WithDescription("a really dumb discord bot for the dinner table.")
+            .AddFields(
+                new EmbedFieldProperties()
+                    .WithName("NetCord Version")
+                    .WithValue(netcordVersion),
+                new EmbedFieldProperties()
+                    .WithName(".NET Version")
+                    .WithValue($"{RuntimeInformation.FrameworkDescription} on {RuntimeInformation.OSDescription}")
+            ).WithFooter(aboutFooter);
+
+        ReplyMessageProperties aboutEmbedMessage = new ReplyMessageProperties()
+            .WithEmbeds([aboutEmbed]);
+
+        await RespondAsync(InteractionCallback.Message(new InteractionMessageProperties() {
+            Embeds = aboutEmbedMessage.Embeds
+        }));
+    }
+}
---
+++ Ashfur/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using NetCord;
+using NetCord.Gateway;
+using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Hosting.Services.Commands;
+
+namespace Ashfur;
+
+internal class Program {
+    static async Task Main(string[] args) {
+        var builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services
+            .AddDiscordGateway(options => {
+                options.Intents = GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.MessageContent;
+                options.Presence = new PresenceProperties(UserStatusType.DoNotDisturb) {
+                    Activities = [
+                        new UserActivityProperties("gay stuff 🏳️‍🌈", UserActivityType.Competing)
+                    ]
+                };
+            })
+            .AddGatewayHandlers(typeof(Program).Assembly)
+            .AddApplicationCommands();
+
+        var host = builder.Build();
+
+        host.AddModules(typeof(Program).Assembly);
+
+        await host.RunAsync();
+    }
+}
