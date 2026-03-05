65803b1fdd5ef92d239f70ab4482e9e8c8c251c2
asoji <git@asoji.gay>
2026-03-05 04:23:24 -0800

    setup mongodb
    Co-authored-by: Storm Anderson <storm@artrinix.xyz>

--- Ashfur/Program.cs
+++ Ashfur/Program.cs
@@ -1,7 +1,6 @@
-﻿using JetBrains.Annotations;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
+using MongoDB.Driver;
 using NetCord;
 using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
@@ -17,8 +16,9 @@ namespace Ashfur;
 
 internal class Program {
     static async Task Main(string[] args) {
-        String serilogOutputTemplate = "[{Timestamp:HH:mm:ss}] [{Level:u3}] [{AssemblyName}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
-        
+        String serilogOutputTemplate =
+            "[{Timestamp:HH:mm:ss}] [{Level:u3}] [{AssemblyName}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Verbose()
             .Enrich.FromLogContext()
@@ -33,7 +33,7 @@ internal class Program {
                     LogEventLevel.Verbose);
             })
             .CreateLogger();
-        
+
         var builder = Host.CreateApplicationBuilder(args);
 
         builder.Services
@@ -49,11 +49,18 @@ internal class Program {
             .AddGatewayHandlers(typeof(Program).Assembly)
             .AddApplicationCommands()
             .AddOsuApiClient(new OsuClientAccessTokenProvider(builder.Configuration["Osu:ClientId"],
-                builder.Configuration["Osu:ClientSecret"]));
+                builder.Configuration["Osu:ClientSecret"]))
+            .AddSingleton<IMongoClient>(service => new MongoClient(builder.Configuration["MongoDb:ConnectionString"]));
 
         var host = builder.Build();
 
         host.AddModules(typeof(Program).Assembly);
+
+        // DEBUG TOOL - LIST COLLECTIONS
+        var collections = (await host.Services.GetService<IMongoClient>()
+            .GetDatabase("ashfur")
+            .ListCollectionNamesAsync()).ToList();
+        Log.Logger.Debug($"Found collections: {collections.Count}\n{String.Join("\n", collections)}");
 
         await host.RunAsync();
     }
---
+++ Ashfur/Utils/TypeHelper.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
+
+namespace Ashfur.Utils;
+
+public static class TypeHelper {
+    extension(string) {
+        [PublicAPI]
+        public static int ToInt(string s) => int.Parse(s);
+    }
+
+    // extension(ConfigurationManager) {
+    //     [PublicAPI]
+    //     public static int ToInt(string s) => int.Parse(s);
+    // }
+}
